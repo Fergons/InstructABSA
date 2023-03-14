cd216882e70c96bb2c80e9acf16e3c0a627d36e0
Fergons <fergons777@gmail.com>
2023-03-14 13:29:31 +0100

    fixed data transformation where duplicates were created

--- InstructABSA/data_prep.py
+++ InstructABSA/data_prep.py
@@ -142,7 +142,6 @@ class DatasetLoader:
         df['labels'] = df[aspect_col].apply(
             lambda x: ', '.join([f"{i[term_key]}:{i[category_label_key]}:{i[polarity_label_key]}" for i in x]))
         print('labels', df['labels'][:10])
-        raise Exception
         df['text'] = df[text_col].apply(lambda x: bos_instruction + x + eos_instruction)
         return df
 
